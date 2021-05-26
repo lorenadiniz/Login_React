69f7085494fcc3a7dd6dce9b3f004cb49cf259fa
Lorena <lorena.martins.1600@gmail.com>
2021-05-25 21:16:53 -0300

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,113 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+function UsuarioLogado(props) {
+  return <h1>Olá usuário!</h1>;
+}
+
+function confirma() {
+
+  let obj = { email: document.getElementById("usuario").value, password: document.getElementById("senha").value };
+  // console.log(obj);
+  return obj;
+}
+function UsuarioNaoLogado(props) {
+
+  return (
+    <div>
+      <h1>Efetue Login</h1>
+      <input type="text" id="usuario" placeholder="Informe o usuário" />
+      <br />
+      <input type="password" id="senha" placeholder="Informe a senha" />
+    </div>
+
+  );
+}
+
+
+function Saudacao(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UsuarioLogado />;
+  }
+  return <UsuarioNaoLogado />;
+}
+
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoggedIn: false };
+  }
+
+  handleLoginClick() {
+    let dados = confirma();
+
+    //let dados = { email: "alex@gmail.com", password: "123456" }
+    dados = JSON.stringify(dados);
+    console.log("dados " + dados);
+    const url = 'https://projeto-integrador-4.herokuapp.com/auth/login';
+
+    fetch(url,
+      {
+        method: "POST",
+        body: dados,
+        headers: {
+          'accept': '*/*',
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(res => res.json())
+      .then(function (res) {
+        console.log(res);
+
+        if (res.token !== undefined) {
+          console.log("ACERTOU");
+        } else { console.log("ERROU"); }
+
+      });
+    this.setState({ isLoggedIn: true });
+  }
+
+  handleLogoutClick() {
+    this.setState({ isLoggedIn: false });
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+
+    return (
+      <div>
+        <Saudacao isLoggedIn={isLoggedIn} />
+
+
+        <div>
+          {isLoggedIn
+            ? <LogoutButton onClick={() => this.handleLogoutClick()} />
+            : <LoginButton onClick={() => this.handleLoginClick()} />
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <LoginControl />,
+  document.getElementById('root')
+);
